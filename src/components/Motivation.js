8ab70f9d01acfe484a0a1dccc6f9e1d6ae3b0ef0
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-08-02 13:50:51 -0400

    Add duty support

--- src/components/Motivation.js
+++ src/components/Motivation.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import {Row} from 'reactstrap';
-import {MotivationBlock} from './index';
+import { Row } from 'reactstrap';
+import { MotivationBlock } from './index';
 
 export const Motivation = () => {
-	return (
-		<div className='w-100'>
-			<Row className='justify-content-end'><h5>MOTIVATIONS</h5></Row>
-			<hr/>
-			<Row className='justify-content-center'>
-				{['Strength', 'Flaw', 'Desire', 'Fear'].map(type =>
-					<MotivationBlock key={type} type={type}/>
-				)}
-			</Row>
-		</div>
-	)
+    return (
+        <div className="w-100">
+            <Row className="justify-content-end">
+                <h5>MOTIVATIONS</h5>
+            </Row>
+            <hr />
+            <Row className="justify-content-center">{['Strength', 'Flaw', 'Desire', 'Fear', 'Duty', 'Duty2'].map(type => <MotivationBlock key={type} type={type} />)}</Row>
+        </div>
+    );
 };
-
