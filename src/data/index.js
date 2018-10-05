a770c1b3956df90d954a777ea3aadf61f2a9f47b
Carl-Hugo Marcotte <contact@carl-hugo.ca>
2018-10-04 20:30:34 -0400

    Patch a Star Wars setting

--- src/data/index.js
+++ src/data/index.js
@@ -15,6 +15,9 @@ import * as talentsCRB from './talents/CRB.json';
 import * as talentsROT from './talents/ROT.json';
 import * as weaponsCRB from './weapons/CRB.json';
 import * as weaponsROT from './weapons/ROT.json';
+import * as skillsCRB from './skills.json';
+import * as skillsSW from './skills-StarWars.json';
+import * as defaultSettings from './settings.json';
 
 export const archetypes = merge.all([archetypesCRB, archetypesROT]);
 export const archetypeTalents = merge.all([archetypeTalentsCRB, archetypeTalentsROT]);
@@ -28,6 +31,32 @@ export const weapons = merge.all([weaponsCRB, weaponsROT]);
 
 export {default as motivations} from './motivations.json';
 export {default as qualities} from './qualities.json';
-export {default as skills} from './skills.json';
-export {default as settings} from './settings.json';
+// export {default as skills} from './skills.json';
+// export {default as settings} from './settings.json';
 export {dataTypes, customDataTypes, chars, diceNames, modifiableAttributes} from './lists';
+
+//
+// Patch the Star Wars setting
+//
+const StarWarsName = "Star Wars";
+defaultSettings.starWars = StarWarsName;
+export const settings = defaultSettings;
+
+// Patch skills to support the Star Wars setting
+export const skills = merge.all([skillsCRB, skillsSW]);
+delete skills.Knowledge; // Delete Knowledge
+addToStarWars(["Astrocartography", "Computers", "Driving", "Gunnery", "Melee", "Piloting", "RangedHeavy", "RangedLight"], skills);
+
+// Patch Archetypes
+addToStarWars(["Bioroid", "Clone", "Robot", "Vanguard", "AnimalisticAlien"], archetypes);
+
+// Patch Careers
+addToStarWars(["Hacker", "FighterPilot", "StarshipCaptain"], careers);
+
+// Patcher
+function addToStarWars(items, obj){
+    for (let index = 0; index < items.length; index++) {
+        const key = items[index];
+        obj[key].setting.push(StarWarsName);
+    }
+}
