4b3efe0103894f8b8ef81f1e2f4015e01552b1bd
Carl-Hugo Marcotte <contact@carl-hugo.ca>
2018-12-13 14:39:52 -0500

    Add the executeRollCommand function
    Replace $RESOURCE_DIR by "functions"

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,12 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const executeRollCommand = functions.firestore.document('botmessages/{userId}/rolls/{autoId}').onCreate((snap, context) => {
+    snap.ref.set(
+        {
+            intercepted: true
+        },
+        { merge: true }
+    );
+});
