306a369f7d3233fa27a4e7000017c352b61a6cc5
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-12-16 20:36:08 -0500

    executeRollCommand now use Discord.js directly (again)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,17 @@
 import * as functions from 'firebase-functions';
 import { config } from './config.secrets';
-import * as request from 'request-promise';
+import { Client, Message, TextChannel } from 'discord.js';
 
 export const executeRollCommand = functions.firestore.document('botmessages/{userId}/rolls/{autoId}').onCreate((snap, context) => {
     const data = snap.data() as RollCommand;
-    const uri = `https://${config.azure.host}/api/QueueARoll?code=${config.azure.key}`;
-
-    const options = {
-        uri,
-        method: 'POST',
-        body: data,
-        json: true
-    };
-    return request(options)
+    return bot
+        .rollSkill(data)
         .then(() => {
             snap.ref.delete(); // TODO: check for 200 OK before deleting
-            console.log('HTTP Request sent!', data);
+            console.log('Roll command executed.', data);
         })
         .catch(reason => {
-            console.error(reason);
-            console.log('options', options);
+            console.error(reason, data);
         });
 });
 
@@ -46,3 +38,37 @@ interface UserInfo {
     email: string;
     uid: string;
 }
+
+class MyDiscordBot {
+    private client!: Client;
+    public rollSkill(rollCommand: RollCommand): Promise<Message | Message[]> {
+        return this.enforceClient().then(() => {
+            const channel = this.client.channels.get(rollCommand.discord.channelId) as TextChannel;
+            const dicesToRoll = computeDices(rollCommand.dices, rollCommand.additionalDices);
+            let rollMsg = `**${rollCommand.characterName}** (${rollCommand.userInfo.displayName}) rolls **${rollCommand.skill}`;
+            if (rollCommand.shortAttribute) {
+                rollMsg += ` (${rollCommand.shortAttribute})`;
+            }
+            rollMsg += '**';
+            return channel.send(`!!roll ${dicesToRoll} "${rollMsg}"`);
+        });
+    }
+    private enforceClient(): Promise<string> {
+        if (this.client) return Promise.resolve('');
+        this.client = new Client();
+        return this.client.login(config.auth.token);
+    }
+}
+
+function computeDices(dices: any, additionalDices: any) {
+    let result = dices;
+    for (const key in additionalDices) {
+        if (additionalDices.hasOwnProperty(key)) {
+            const amount = additionalDices[key];
+            result += key.repeat(amount);
+        }
+    }
+    return result;
+}
+
+const bot = new MyDiscordBot();
